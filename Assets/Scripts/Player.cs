038b4ceff827a1a8554848a0025c74f13af08e2b
Daniel Dias <dandanielddp@gmail.com>
2024-04-09 00:56:45 -0300

    Pular ao encostar no chão

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float force = 100f;
+    public Collider colPlataform;
+
+    Ray ray;
+    RaycastHit hit;
+
+
+    public float contador;
+    public bool podeContar = false;
+    public float cooldown = 0.1f;
+    public float speed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        speed = rb.velocity.y;
+         ray = new Ray(transform.position, -transform.up * 100f);
+
+         if (Physics.Raycast(ray, out hit))
+         {
+             colPlataform = hit.collider.gameObject.GetComponent<Collider>();
+
+             colPlataform.isTrigger = false;
+        }
+        else
+        {
+            colPlataform.isTrigger = true;
+        }
+
+
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (rb.velocity.y == 0)
+        {
+            rb.AddForce(transform.up * force, ForceMode.Impulse);
+
+        }
+
+    }
+}
